a1dbcb3703f93334e7834deeca70c20e85162627
Yuri Martins <cyuri.martins@gmail.com>
2018-02-27 19:36:04 -0300

    Create aula02_Scanner.java

---
+++ aula02_Scanner.java
@@ -0,0 +1,19 @@
+package entrada;
+
+import java.util.*; 
+public class AppEntrada {
+
+    public static void main(String[] args) {
+            Scanner entrada = new Scanner(System.in);
+            float av1, av2;            
+            
+            System.out.print("Informe a sua AV1: ");
+            av1 = entrada.nextFloat();
+            
+            System.out.print("Informe a sua AV2: ");
+            av2 = entrada.nextFloat();  
+                    
+                    
+    }
+    
+}
