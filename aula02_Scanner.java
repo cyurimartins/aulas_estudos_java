24ff10691b9a57f712a4755869d39e15a2a9b0b7
Yuri Martins <cyuri.martins@gmail.com>
2018-02-27 20:52:52 -0300

    Update aula02_Scanner.java

--- aula02_Scanner.java
+++ aula02_Scanner.java
@@ -1,3 +1,18 @@
+/*Entrada de dados via scanner
+
+Podemos utilizar o scanner com o objetivo de solicitar ao usuário uma entrada de dados. Este componente faz parte da biblioteca Java.util e para sua utilização ele precisa ser importado. Pois é um objeto pertencente ao java, sintaxe básica.
+
+Sintaxe basica: primitivo e sintaxe C. 
+
+Scanner sc = new Scanner (System.in)
+
+Biblioteca: java.util.scanner
+
+Para acessar o componente: sc.
+string: sc.next
+inteiro: sc.nextint
+double: sc.nextdouble*/
+
 package entrada;
 
 import java.util.*; 
