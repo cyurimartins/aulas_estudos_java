500cfa652075087a7057476835f3ed4e6bd30d76
Yuri Martins <cyuri.martins@gmail.com>
2018-03-18 12:38:01 -0300

    Create entradaDados.java

---
+++ entradaDados.java
@@ -0,0 +1,16 @@
+package entradadedados;
+
+import java.util.Scanner;
+
+public class EntradaDeDados {
+
+
+    public static void main(String[] args) {
+        Scanner teclado =new Scanner(System.in);
+        String nome = teclado.nextLine();
+        float nota = teclado.nextFloat();
+        
+        System.out.format("A nota de %s é %.1f: \n", nome, nota);
+    }
+    
+}
