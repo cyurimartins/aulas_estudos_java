6983b173ed64cbf1e15af5de96d17983b4be1b7e
Yuri Martins <cyuri.martins@gmail.com>
2018-03-19 23:56:03 -0300

    Create NumeroRandom.java

---
+++ NumeroRandom.java
@@ -0,0 +1,14 @@
+package operadoresarit;
+
+public class OperadoresArit {
+   
+    public static void main(String[] args) {
+        
+        //GERAR NUMERO ALEATORIO ENTRE 5 E 10
+        double ale = Math.random();
+        int n = (int) (5 + ale * (10-5));
+        System.out.println(n);
+        
+    }
+    
+}
