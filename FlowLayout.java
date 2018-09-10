742df6daa50c35346a76de1aebff88f057bd12d4
Yuri Martins <cyuri.martins@gmail.com>
2018-09-10 00:11:23 -0300

    Create FlowLayout.java

---
+++ FlowLayout.java
@@ -0,0 +1,30 @@
+package InterfaceGrafica;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Layout extends JFrame{
+	
+	JButton botao = new JButton("Teste");
+	JButton botao2 = new JButton("Teste");
+	
+	public Layout() {
+
+		setLayout(new FlowLayout(FlowLayout.LEFT)); // PARA PASSAR OS COMPONENTES A ESQUERDA
+		add(botao);
+		add(botao2);
+		
+		setTitle("Exemplo Border Layout");
+		setSize(700,600);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+	}
+	
+	public static void main (String[] args) {
+		new Layout();
+	}
+}
